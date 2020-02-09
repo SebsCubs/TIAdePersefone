166a431ed9729e6478351318844fef39f60b011a
SebsCubs <ascespedesc@unal.edu.co>
2020-02-09 14:37:31 -0500

    Added examples for method definition and it's use in main file

--- data_enhancement/rotation__span.py
+++ data_enhancement/rotation__span.py
@@ -1,2 +1,10 @@
 #Python module add functions here to import on main
-        
+
+def rotation_create ():
+    #Here to receive one image and to create two rotated
+    print("Rotadito")
+
+def wrap_create ():
+    #Here to receive one image and create 2 with random wrapping
+    print("Wrappeadito")
+
--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+#Main routine execution for latin handwritten text processing
+#Técnicas de inteligencia artificial: 2019-2
+#Universidad Nacional de Colombia
+
+#How to import packages:
+#   import data_enhancement.rotation_span
+#   data_enhancement.rotation__span
+#either
+#   from data_enhancement import rotation__span
+#   rotation__span
+#either
+#   import data_enhancement.rotation__span
+#   data_enhancement.rotation__span.printPrueba(1)
+
+import numpy as np
+from matplotlib import pyplot as plt
+import cv2
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
+
+####################_Main routine_##################################
+
+
 
